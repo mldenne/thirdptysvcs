fb4ca70bef2952dfcac103aa969b5514a3d3962c
Marie Denney <mldenne@gmail.com>
2016-08-02 14:55:45 -0400

    Updated email_processor

--- app/models/email_processor.rb
+++ app/models/email_processor.rb
@@ -5,7 +5,7 @@ class EmailProcessor
 
   def process
     Rails.logger.info @email.attachments.inspect
-    post = Post.create!({ title: @email.subject, body: @email.body, from_email: @email.from[:email] })
+    post = Post.create!({ caption: @email.subject, image_id: @email.attachment.first })
     Pusher.trigger('test_channel', 'my_event', PostSerializer.new(post).as_json)
   end
 end
