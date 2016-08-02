7681f20a63105f72aafe1d8f9e01138d37d75569
Marie Denney <mldenne@gmail.com>
2016-08-02 16:08:20 -0400

    Updated post.rb with refile info, updated Posts Controller with post params

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -3,3 +3,9 @@ Refile.configure do |config|
   connection = lambda { |&blk| ActiveRecord::Base.connection_pool.with_connection { |con| blk.call(con.raw_connection) } }
   config.store = Refile::Postgres::Backend.new(connection)
 end
+
+if Rails.env.production?
+  Refile.cdn_host = 'https://serene-basin-53447.herokuapp.com'
+else
+  Refile.cdn_host = 'https://775fbd34.ngrok.io'
+end
