1caadc69f2cad12967521bcb27039feb088a86d9
Marie Denney <mldenne@gmail.com>
2016-08-02 14:16:42 -0400

    Added route for email_processor

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   resources :posts
+
+  post '/email_processor' => 'griddler/emails#create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
